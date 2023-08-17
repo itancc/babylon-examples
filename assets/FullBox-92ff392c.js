29a4106d290d19a847c5ead8e9af1d7a5257d245
itancc <itancc@users.noreply.github.com>
2023-08-17 09:01:35 +0000

    deploy: 7a211293100fb8f47e570c56514f04b441bdf55c

---
+++ assets/FullBox-92ff392c.js
@@ -0,0 +1,1 @@
+import{s as a}from"./index-e175da1f.js";const s=a("div")(({theme:o})=>({width:"100%",height:"100%",backgroundColor:o.palette.background.paper})),d=a("canvas")({width:"100%",height:"100%",padding:0,margin:0});export{s as B,d as C};
