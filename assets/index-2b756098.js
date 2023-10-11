866409cbf9f92f2a0027b191046c23d1216d4ee8
itancc <itancc@users.noreply.github.com>
2023-10-11 08:43:04 +0000

    deploy: 9e130373495dfaecd669ca17591b4e5eee8fe152

---
+++ assets/index-2b756098.js
@@ -0,0 +1,1 @@
+import{l as t,j as s}from"./index-7a5f133a.js";import{u as m,F as a}from"./useExamples-8de662f8.js";const x=()=>{const{name:n}=t(),e=m().find(o=>o.name===n);return s.jsx(a,{children:e?s.jsx(e.component,{}):null})};export{x as default};
--- assets/index-cf23dc11.js
+++
@@ -1,1 +0,0 @@
-import{l as t,j as s}from"./index-1e15a5c8.js";import{u as m,F as a}from"./useExamples-191783c4.js";const x=()=>{const{name:n}=t(),e=m().find(o=>o.name===n);return s.jsx(a,{children:e?s.jsx(e.component,{}):null})};export{x as default};
